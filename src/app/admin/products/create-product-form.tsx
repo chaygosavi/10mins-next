937e1736211dbd6f97b29a65a639b4acbf663c2c
Chaitanya Gosavi <chaitanyagosavi@Chaitanyas-Mac-mini.local>
2024-09-11 00:10:27 +0530

    added form

--- src/app/admin/products/create-product-form.tsx
+++ src/app/admin/products/create-product-form.tsx
@@ -2,14 +2,95 @@ import React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { productSchema } from "@/lib/validators/productSchema";
-
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
 type Props = {};
+type FormValues = z.input<typeof productSchema>;
 
 const CreateProductForm = (props: Props) => {
-  const form = useForm({
+  const form = useForm<z.infer<typeof productSchema>>({
     resolver: zodResolver(productSchema),
+    defaultValues: {
+      name: "",
+      description: "",
+      price: 0,
+    },
   });
-  return <div>CreateProductForm</div>;
+
+  const fileRef = form.register("image");
+
+  const handleSubmit = (values: FormValues) => {};
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="e.g. Chocobar" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Textarea {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="image"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Image</FormLabel>
+              <FormControl>
+                <Input type="file" {...fileRef} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="price"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Price</FormLabel>
+              <FormControl>
+                <Input type="number" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button className="w-full">Create</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default CreateProductForm;
